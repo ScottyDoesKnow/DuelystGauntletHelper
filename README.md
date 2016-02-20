13575c8039e94950be845b1e48cfe34907198e9e
Scott Sherin <scott.sherin@mc-cm.com>
2016-02-20 17:44:28 -0500

    Added error handling. Check for an error.log file if the program crashes.

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # DuelystGauntletHelper
 Overlays CynosureGG's card ratings and notes when you pick cards for gauntlet.
 
-## Screenshots ##
-http://imgur.com/a/XWOMf
+## Screenshot ##
+http://i.imgur.com/auQa6Kp.png
 
 ## Requirements ##
 - Microsoft .NET Framework 4.5 - https://www.microsoft.com/en-ca/download/details.aspx?id=30653
 - Visual C++ Redistributable for Visual Studio 2015 - https://www.microsoft.com/en-us/download/details.aspx?id=48145
+- The x86 version of Visual C++ may be required, whether you're on a 64 bit system or not.
 
 ## CynosureGG ##
 Gauntlet Tier List: https://docs.google.com/document/d/1JKJ5fjvwhchefhJcrQDOgRIwqaDKpj13PRstVyn7mG0/pub
--- src/GauntletHelper/App.xaml.cs
+++ src/GauntletHelper/App.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,8 +15,12 @@ namespace GauntletHelper
     /// </summary>
     public partial class App : Application
     {
+        private const string ERROR_LOG = "error.log";
+        private const long ERROR_LOG_MAX = 1024 * 1024; // 1 MB
+
         static App()
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             DispatcherHelper.Initialize();
         }
 
@@ -23,6 +28,27 @@ namespace GauntletHelper
         {
             base.OnStartup(e);
             ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            LogException((Exception)e.ExceptionObject);
+        }
+
+        private static void LogException(Exception ex)
+        {
+            if (File.Exists(ERROR_LOG) && new FileInfo(ERROR_LOG).Length > ERROR_LOG_MAX)
+            {
+                File.Copy(ERROR_LOG, ERROR_LOG + ".old", true);
+                File.Delete(ERROR_LOG);
+            }
+
+            using (TextWriter writer = new StreamWriter("error.log", true))
+            {
+                writer.WriteLine(DateTime.UtcNow);
+                writer.WriteLine(ex.ToString());
+                writer.WriteLine();
+            }
         }
     }
 }
